80f4eabe366b90a4aaf8b3b1463b8f1bc833b583
fexd12 <felipe.demelo@hotmail.com>
2020-04-20 13:03:27 -0300

    arrumado exercicio A

--- rk4/exercicioA.py
+++ rk4/exercicioA.py
@@ -1,20 +1,35 @@
 import math as Math
 import matplotlib.pyplot as plt
 
+def kUm1(x1, x2):
+    return 0.02 * x2 - 0.03 * x1
 
+def kDois1(x1, x2, k1):
+    return 0.02 * (x2 + k1/2) - 0.03 * (x1 + k1/2)
 
+def kTres1(x1, x2, k2):
+    return 0.02 * (x2 + k2/2) - 0.03 * (x1 + k2/2)
 
+def kQuatro1(x1, x2, k3):
+    return 0.02 * (x2 + k3) - 0.03 * (x1 + k3)
 
+def kUm2(x1, x2):
+    return 0.03 * x1 - (0.01 + 0.02) * x2
 
+def kDois2(x1, x2, k1):
+    return 0.03 * (x1 + k1 / 2) - (0.01 + 0.02) * (x2 + k1 / 2)
 
+def kTres2(x1, x2, k2):
+    return 0.03 * (x1 + k2 / 2) - (0.01 + 0.02) * (x2 + k2 / 2)
 
+def kQuatro2(x1, x2, k3):
+    return 0.03 * (x1 + k3) - (0.01 + 0.02) * (x2 + k3)
 
+# def fx1(x1,x2):
+#     return 0.02 * x2  - 0.03*x1
 
-def fx1(x1,x2):
-    return 0.02 * x2  - 0.03*x1
-
-def fx2(x2,x1):
-    return 0.03 *x1 - (0.02 + 0.01)*x2
+# def fx2(x1,x2):
+#     return 0.03 *x1 - (0.02 + 0.01)*x2
 
 h = 0.001
 t = 0.1
@@ -22,36 +37,40 @@ k11 = 0
 k12 = 0
 k13 = 0
 k14 = 0
+
 k21 = 0
 k22 = 0 
 k23 = 0
 k24 = 0
+
 x1 = 100 
 x2 = 10
 auxa = 0 
-auxc = 0 
-erro =500
+auxc = 0
+
+erro =0
 
 plt.ioff()
 
 at=[]
 ax1=[]
 ax2=[]
-while erro > 4e-5:
+while t<150:
 
     auxa = x2
     auxc = x1
-    k11 = h*fx1(x1,x2)
-    k21 = h*fx2(x2,x1)
-    
-    k12 = h*fx1(t + h/2, x2 + k11/2)
-    k22 = h*fx2(t + h/2, x1 + k21/2)
 
-    k13 = h*fx1(t + h/2, x2 + k12/2)
-    k23 = h*fx2(t + h/2, x1 + k22/2)
+    k11 = h*kUm1(x1, x2)
+    k21 = h*kUm2(x1, x2)
 
-    k14 = h*fx1(t + h, x2 + k13)
-    k24 = h*fx2(t + h, x1 + k23)
+    k12 = h*kDois1(x1, x2, k11)
+    k22 = h*kDois2(x1, x2, k21)
+
+    k13 = h*kTres1(x1, x2, k12)
+    k23 = h*kTres2(x1, x2, k22)
+
+    k14 = h*kQuatro1(x1, x2, k13)
+    k24 = h*kQuatro2(x1, x2, k23)
 
     print('t:{} || x1:{} || x2:{} || erro:{}'.format(t,x1,x2,erro))
     at.append(t)
