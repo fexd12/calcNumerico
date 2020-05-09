8a883ad3fcd3441e02ae9981741f5a67d80a14e5
fexd12 <felipe.demelo@hotmail.com>
2020-05-09 13:44:22 -0300

    atualizado prova

--- prova/modelo2.py
+++ prova/modelo2.py
@@ -23,53 +23,41 @@ def abrirArquivo(path):
         x.append(e)
     return x
 
-def kUm1(x1, x4):
-    return k11 * x4 - (k31+k21)*x1
+def kUm1(x1, x3):
+    return k21 * x3 - (k11+k31)*x1
 
-def kDois1(x1,x4, k1):
-    return k11 * (x4+k1/2) - (k31+k21)*(x1 + k1/2)
+def kDois1(x1,x3, k1):
+    return k21 * (x3+k1/2) - (k11+k31)*(x1+k1/2)
 
-def kTres1(x1, x4, k2):
-    return k11 * (x4+k2/2) - (k31+k21)*(x1 + k2/2)
+def kTres1(x1, x3, k2):
+    return k21 * (x3+k2/2) - (k11+k31)*(x1+k2/2)
 
-def kQuatro1(x1, x4, k3):
-    return k11 * (x4+k3) - (k31+k21)*(x1 + k3)
+def kQuatro1(x1, x3, k3):
+    return k21 * (x3+k3) - (k11+k31)*(x1+k3)
 
 def kUm2(x1):
-    return k31 * x1
+    return k11 * x1
 
 def kDois2(x1, k1):
-    return k31 * (x1+k1/2)
+    return k11 * (x1+k1/2)
 
 def kTres2(x1, k2):
-    return k31 * (x1+k2/2)
+    return k11 * (x1+k2/2)
 
 def kQuatro2(x1, k3):
-    return k31 * (x1+k3)
+    return k11 * (x1+k3)
 
-def kUm3(x1):
-    return k21 * x1
+def kUm3(x1,x3):
+    return -1 * k21 * x3 + k31 * x1
 
-def kDois3(x1, k1):
-    return k21 * (x1+k1/2)
+def kDois3(x1,x3, k1):
+    return -1 * k21 * (x3+k1/2) + k31 * (x1+k1/2)
 
-def kTres3(x1, k2):
-    return k21 * (x1+k2/2)
+def kTres3(x1,x3, k2):
+    return -1 * k21 * (x3+k2/2) + k31 * (x1+k2/2)
 
-def kQuatro3(x1, k3):
-    return k21 * (x1+k3)
-
-def kUm4(x4):
-    return -1* k11 * x4
-
-def kDois4(x4, k1):
-    return -1* k11 * (x4+k1/2)
-
-def kTres4(x4, k2):
-    return -1* k11 * (x4+k2/2)
-
-def kQuatro4(x4, k3):
-    return -1* k11 * (x4+k3)
+def kQuatro3(x1,x3, k3):
+    return -1 * k21 * (x3+k3) + k31 * (x1+k3)
 
 url=["https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",
 "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv",
@@ -80,106 +68,90 @@ for i in range(0,3):
     
 dados_confirmed = 'dados/time_series_covid19_confirmed_global.csv'
 dados_deaths= 'dados/time_series_covid19_deaths_global.csv'
-dados_recovered= 'dados/time_series_covid19_recovered_global.csv'
 
 brasil_confirmed = abrirArquivo(dados_confirmed)
 brasil_deaths = abrirArquivo(dados_deaths)
-brasil_recovered = abrirArquivo(dados_recovered)
 
 casos = 209000000 * 0.7 - int(brasil_confirmed[len(brasil_confirmed)-1])
 
-k11 = delta(brasil_confirmed) / casos
-k31 = delta(brasil_deaths) / int(brasil_confirmed[len(brasil_confirmed)-2])
-k21 = delta(brasil_recovered) / int(brasil_confirmed[len(brasil_confirmed)-2])
+k31 = (casos / delta(brasil_confirmed)) - (delta(brasil_confirmed) / casos)
+k11 = delta(brasil_deaths) / int(brasil_confirmed[len(brasil_confirmed)-2])
+k21 = (delta(brasil_confirmed) / casos) - (casos / delta(brasil_confirmed))
+
 h = 1
 t = 0
 
 rk11 = 0
 rk12 = 0
 rk13 = 0
-rk14 = 0
 
 rk21 = 0
 rk22 = 0
 rk23 = 0
-rk24 = 0
 
 rk31 = 0
 rk32 = 0
 rk33 = 0
-rk34 = 0
 
 rk41 = 0
 rk42 = 0
 rk43 = 0
-rk44 = 0
 
 x1= int(brasil_confirmed[len(brasil_confirmed)-1])
 x2= int(brasil_deaths[len(brasil_deaths)-1])
-x3= int(brasil_recovered[len(brasil_recovered)-1])
-x4= casos
+x3= casos
 
 auxX1 = 0
 auxX2 = 0
 auxX3 = 0
-auxX4 = 0
 erro = 0
 
 matplotlib.pyplot.ioff()
 graphT = []
 graphX1 = []
 graphX2 = []
-graphX3 = []
 
 while t < 40:
     auxX1 = x1
     auxX2 = x2
     auxX3 = x3
-    auxX4 = x4
 
-    rk11 = h*kUm1(x1,x4)
+    rk11 = h*kUm1(x1,x3)
     rk21 = h*kUm2(x1)
-    rk31 = h*kUm3(x1)
-    rk41 = h*kUm4(x4)
+    rk31 = h*kUm3(x1,x3)
 
-    rk12 = h*kDois1(x1,x4, rk11)
+    rk12 = h*kDois1(x1,x3, rk11)
     rk22 = h*kDois2(x1, rk21)
-    rk32 = h*kDois3(x1, rk31)
-    rk42 = h*kDois4(x4, rk41)
+    rk32 = h*kDois3(x1,x3, rk31)
 
-    rk13 = h*kTres1(x1,x4, rk12)
+    rk13 = h*kTres1(x1,x3, rk12)
     rk23 = h*kTres2(x1, rk22)
-    rk33 = h*kTres3(x1, rk32)
-    rk43 = h*kTres4(x4, rk42)
+    rk33 = h*kTres3(x1,x3, rk32)
 
-    rk14 = h*kQuatro1(x1,x4, rk13)
+    rk14 = h*kQuatro1(x1,x3, rk13)
     rk24 = h*kQuatro2(x1, rk23)
-    rk34 = h*kQuatro3(x1, rk33)
-    rk44 = h*kQuatro4(x4, rk43)
+    rk34 = h*kQuatro3(x1,x3, rk33)
 
-    print(f't:{t} || C:{x1} || M:{x3} || R:{x4}')
+    print(f't:{t} || C:{x1} || M:{x2}')
 
     graphT.append(t)
     graphX1.append(x1)
     graphX2.append(x2)
-    graphX3.append(x3)
 
     x1 = x1 + (rk11 + 2 * rk12 + 2 * rk13 + rk14)/6
     x2 = x2 + (rk21 + 2 * rk22 + 2 * rk23 + rk24)/6
     x3 = x3 + (rk31 + 2 * rk32 + 2 * rk33 + rk34)/6
-    x4 = x4 + (rk41 + 2 * rk42 + 2 * rk43 + rk44)/6
 
-    k11 = (x1-auxX1) / x4
-    k31 = (x2-auxX2) / auxX1
-    k21 = (x3-auxX3) / auxX1
+    k21= ((x1-auxX1) / x3) - ((x3-auxX3) / x1)
+    k11 = (x2-auxX2) / auxX1
+    k31 = ((x3-auxX3) / x1) - ((x1-auxX1) / x3)
 
-    erro = (x1 - auxX1) / auxX1 + (x2 - auxX2) / auxX2 + (x3 - auxX3) / auxX3 + (x4 - auxX4) / auxX4
+    erro = (x1 - auxX1) / auxX1 + (x2 - auxX2) / auxX2 + (x3 - auxX3) / auxX3
     t = t + h
 
 matplotlib.pyplot.plot(graphT,graphX1,label='C(t)')
 matplotlib.pyplot.plot(graphT,graphX2,label='M(t)')
-matplotlib.pyplot.plot(graphT,graphX3,label='R(t)')
 matplotlib.pyplot.xlabel('tempo (t)')
-matplotlib.pyplot.ylabel('valores (C(t) M(t) e R(t))')
+matplotlib.pyplot.ylabel('valores (C(t) M(t)')
 matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
